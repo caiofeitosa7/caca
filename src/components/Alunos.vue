586c19abc353c58e959dfe1f23de560748c398b9
Caio Feitosa <franciscocaiof@gmail.com>
2024-03-04 07:15:59 -0300

    feat: listagem de alunos e filtro funcional

--- src/components/Alunos.vue
+++ src/components/Alunos.vue
@@ -5,6 +5,7 @@
         data() {
             return {
                 listaAlunos: {},
+                quantAlunos: 0,
                 url: 'http://192.168.0.27:5000/listar_alunos'
             };
         },
@@ -12,19 +13,25 @@
             async carregarAlunos() {
                 try {
                     const response = await axios.get(this.url);
-                    this.listaAlunos = response.data;
+                    this.listaAlunos = response.data.alunos;
+                    this.quantAlunos = response.data.quantidade;
                 } catch (error) {
                     console.error('Erro ao carregar voluntários:', error);
                 }
             },
             async filtrarAlunos(){
-                try {
-                    // const response = await axios.post(this.url, {
-                    //     nome_voluntario: document.getElementById("nome").value
-                    // });
-                    // this.listaAlunos = response.data;
+                let divFiltro = document.getElementById("filtro");
+                let camposInput = divFiltro.querySelectorAll("input, select");
 
-                    alert("filtrou alunos")
+                let dados = {};
+                camposInput.forEach(function (campo) {
+                    dados[campo.id] = campo.value;
+                });
+
+                try {
+                    const response = await axios.post(this.url, dados);
+                    this.listaAlunos = response.data.alunos;
+                    this.quantAlunos = response.data.quantidade;
                 } catch (error) {
                     console.error('Erro ao carregar voluntários:', error);
                 }
@@ -34,6 +41,9 @@
 			},
             limparFiltro(){
                 document.getElementById("nome").value = "";
+                document.getElementById("sexo").value = "M";
+                document.getElementById("idade1").value = "";
+                document.getElementById("idade2").value = "";
                 this.carregarAlunos();
             }
         },
@@ -47,10 +57,29 @@
     <div class="container-alunos m-3">
         <div class="container-filtro is-flex is-align-items-center is-justify-content-space-between mb-4">
             <div id="filtro" class="is-flex is-align-items-center">
-                <label for="nome" class="font-weight-600 mr-2">
-                    Nome:
-                </label>
-                <input id="nome" class="input is-success">
+                <div class="is-flex is-align-items-center mr-3">
+                    <label for="nome" class="font-weight-600 mr-2">
+                        Nome:
+                    </label>
+                    <input id="nome" class="input is-success">
+                </div>
+                <div class="is-flex is-align-items-center mr-3">
+                    <label for="sexo" class="font-weight-600 mr-2">Sexo:</label>
+                    <div class="select is-success is-rounded">
+                        <select id="sexo">
+                            <option value="M">M</option>
+                            <option value="F">F</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="is-flex is-align-items-center">
+                    <label for="idade1" class="font-weight-600 mr-2">
+                        Idade:
+                    </label>
+                    <input id="idade1" type="number" class="campoIdade input is-success" min="0" max="18">
+                    <span class="mx-2">à</span>
+                    <input id="idade2" type="number" class="campoIdade input is-success" min="0" max="18">
+                </div>
                 <button id="btnPesquisar" class="is-flex is-align-items-center" @click="filtrarAlunos">
                     <i class='bx bx-search-alt mx-2'></i>
                 </button>
@@ -66,11 +95,16 @@
             <div class="cabecalho columns is-mobile mb-0">
                 <span class="column is-1 text-align-center">Código</span>
                 <span class="column is-4">Nome</span>
+                <span class="column is-2">CPF</span>
+                <span class="column is-1 text-align-center">Idade</span>
+                <span class="column is-1 text-align-center">Sexo</span>
             </div>
             <div class="linha clicavel columns is-mobile" v-for="(aluno, index) in this.listaAlunos" :key="index">
                 <span class="column is-1 text-align-center">{{ aluno.codigo }}</span>
                 <span class="column is-4">{{ aluno.nome }}</span>
-
+                <span class="column is-2">{{ aluno.cpf }}</span>
+                <span class="column is-1 text-align-center">{{ aluno.idade }}</span>
+                <span class="column is-1 text-align-center">{{ aluno.sexo }}</span>
             </div>
         </div>
         <p class="mt-4" v-else>Não há alunos para exibir!</p>
@@ -99,6 +133,18 @@
         #filtro {
             label {
                 text-wrap: nowrap;
+            }
+
+            .select {
+                background-color: white;
+            }
+
+            #nome {
+                width: 120px;
+            }
+
+            .campoIdade {
+                width: 60px;
             }
 
             #btnLimparFiltro {
