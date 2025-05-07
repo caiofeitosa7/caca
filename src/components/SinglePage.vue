703e507a5161d3331f22e9fb0c0ff5deb71c119a
Caio Feitosa <68925245+caiofeitosa7@users.noreply.github.com>
2025-05-07 14:29:51 -0300

    Update SinglePage.vue
    Atualizando rodapé da singlepage

--- src/components/SinglePage.vue
+++ src/components/SinglePage.vue
@@ -391,7 +391,7 @@
             </div>
         </section>
         <footer class="is-flex is-justify-content-center py-4">
-            <span>CACA © 2024 - Todos os direitos reservados.</span>
+            <span>CACA © 2025 - Todos os direitos reservados.</span>
         </footer>
     </main>
 </template>
